1f8e8197015b8b50c99309928650b9c31fb7a940
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-06-26 15:51:38 -0400

    union

--- spike_psvae/newton_motion_est.py
+++ spike_psvae/newton_motion_est.py
@@ -450,7 +450,7 @@ def xcorr_windows(
     xcorr_kw=default_xcorr_kw,
     device=None,
 ):
-    xcorr_kw = default_xcorr_kw | xcorr_kw
+    xcorr_kw = dict(default_xcorr_kw.items() | xcorr_kw.items())
 
     if device is None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
